7721a73eaeeb19cc6ba71f4bb68f5e1f1460dc2e
cesarhcq <cesar@fem.unicamp.br>
2020-07-29 18:38:38 -0300

    filter encoder updated

--- arduino_controller/src/robot_specs_polulo.h
+++ arduino_controller/src/robot_specs_polulo.h
@@ -4,7 +4,7 @@
 #define ENCODER_LEFT_PIN 21  // Encoder Left PIN
 #define ENCODER_RIGHT_PIN 20  // Encoder Right PIN
 #define ENCODER_PULSE_LEFT 0.1 //2.5 0.2 0.15
-#define ENCODER_PULSE_RIGH 0.25 //0.5 0.2 0.15
+#define ENCODER_PULSE_RIGH 0.05 //0.5 0.2 0.15 0.25
 #define ENCODER_FILTER 10
 
 #define L 0.42 // distance between axes m
